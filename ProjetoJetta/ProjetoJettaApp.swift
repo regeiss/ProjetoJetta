df29f0a50f487925af5aac7d54643d57d283b3d5
Roberto <roberto.geiss.nh@icloud.com>
2023-05-11 19:45:53 -0300

    Initial Commit

---
+++ ProjetoJetta/ProjetoJettaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjetoJettaApp.swift
+//  ProjetoJetta
+//
+//  Created by Roberto Edgar Geiss on 11/05/23.
+//
+
+import SwiftUI
+
+@main
+struct ProjetoJettaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
