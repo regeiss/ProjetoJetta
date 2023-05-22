0fde0d8604fbeb0ef87b9ad1cf2329f5aba70cfd
Roberto Edgar Geiss <15005694+regeiss@users.noreply.github.com>
2023-05-22 16:53:19 -0300

    Update AbastecimentoModel.swift

--- ProjetoJetta/Code/Features/Abastecimento/Model/AbastecimentoModel.swift
+++ ProjetoJetta/Code/Features/Abastecimento/Model/AbastecimentoModel.swift
@@ -18,8 +18,10 @@ class Abastecimento: Object, ObjectKeyIdentifiable
     @Persisted var valorTotal: Double 
     @Persisted var completo: Bool
     @Persisted var media: Double
-//    let doPosto: Posto
-//    let doCarro: Carro
+    // Relacionamentos
+    @Persisted var doPosto: Posto?
+    @Persisted var doCarro: Carro?
+    
     convenience init(km: Int32, data: Date, valorLitro: Double, valorTotal: Double, completo: Bool, media: Double)
     {
         self.init()
@@ -29,5 +31,40 @@ class Abastecimento: Object, ObjectKeyIdentifiable
         self.valorTotal = valorTotal
         self.completo = completo
         self.media = media
+    }
+}
+
+// ================================================================================================================================================================= //
+class Posto: Object, ObjectKeyIdentifiable
+{
+    @Persisted(primaryKey: true) var id: ObjectId
+    @Persisted var nome: String
+    @Persisted var logo: NSData
+
+    convenience init(nome: String, logo: NSData)
+    {
+        self.init()
+        self.nome = nome
+        self.logo = logo
+    }
+}
+
+class Perfil: Object, ObjectKeyIdentifiable
+{
+    @Persisted(primaryKey: true) var id: ObjectId
+    @Persisted var  nome: String
+    @Persisted var  email: String
+    @Persisted var  ativo: Bool
+    @Persisted var  padrao: Bool
+    // Relacionamento
+    @Persisted var veiculos: List<Veiculo>
+
+    convenience init(nome: String, email: String, ativo: Bool, padrao: Bool)
+    {
+        self.init()
+        self.nome = nome
+        self.email = email
+        self.ativo = ativo
+        self.padrao = padrao
     }
 }
