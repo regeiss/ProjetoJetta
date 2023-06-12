5d3fa8c858d7501e68c4d96f83d40f1e97ab5139
Roberto <roberto.geiss.nh@icloud.com>
2023-06-12 20:25:23 -0300

    ajustes coredata

--- ProjetoJetta/Code/Features/Common/AppState.swift
+++ ProjetoJetta/Code/Features/Common/AppState.swift
@@ -11,8 +11,8 @@ import SwiftUI
 class AppState
 {
     var ultimaKM: Int32
-    var carroAtivo: Veiculo?
-    var perfilAtivo: Perfil?
+//    var carroAtivo: Veiculo?
+//    var perfilAtivo: Perfil?
     var postoPadrao: Posto?
     
     static var shared = AppState()
